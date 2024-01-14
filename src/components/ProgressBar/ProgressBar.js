46c9fc9253ae51167c118630f64a7b804d9fc580
Jamhanpar <james.hansung.park@gmail.com>
2024-01-14 16:42:45 -0500

    base progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,38 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const ProgressBar = ({ value, size, color, ariaLabel }) => {
+  return (
+    <Wrapper>
+      <Bar
+        role='progressbar'
+        size={size}
+        value={value}
+        aria-valuenow={value}
+        aria-label={ariaLabel}
+        style={{
+          '--color': COLORS[color],
+          '--width': `${value}% - 8px`,
+          '--height':
+            size === 'large' ? '16px' : size === 'medium' ? '12px' : '8px',
+        }}
+      />
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  background-color: var(--color);
+  width: var(--width);
+  height: var(--height);
+  border-radius: ${props => (props.value >= 98 ? '4px' : '4px 0px 0px 4px')};
+  margin: ${props => (props.size === 'large' ? '4px' : '0px')};
+`;
