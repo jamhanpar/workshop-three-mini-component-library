3de1406dfb74c4498521e136b7618b0c650bf3f5
Jamhanpar <james.hansung.park@gmail.com>
2024-01-17 19:25:55 -0500

    select button

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,70 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+export default function Select({ label, value, onChange, children }) {
   const displayedValue = getDisplayedValue(value, children);
+  const iconSize = 24;
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--iconSize': iconSize + 'px' }}>
+          <Icon id='chevron-down' strokeWidth={1} size={iconSize} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
-};
+}
 
-export default Select;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+
+  /* Make element fill the entire wrapper */
+  height: 100%;
+  width: 100%;
+
+  /* Make the element still clickable, but not visible */
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  overflow: hidden;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--iconSize);
+  height: var(--iconSize);
+  pointer-events: none;
+`;
